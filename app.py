3cb34968db2d7e64b9803dd70a3cef9d92de781d
carloferri66 <carloferri66@gmail.com>
2026-02-17 14:16:06 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pandas as pd
 st.title("Test di Connessione Rapida 🚵‍♂️")
 
 # INCOLLA QUI IL LINK CHE HAI COPIATO DOPO AVER PUBBLICATO SUL WEB
-URL_PUBBLICO = "IL_TUO_LINK_QUI"
+URL_PUBBLICO = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQWs2Kdjudkb5H16vvEFDuZ7BA4Nmh7oowDZy29W1JFfExJM5BhZwHDQsjoKU82mQ/pub?output=csv"
 
 try:
     # Proviamo a leggere direttamente il CSV dal link pubblico
@@ -14,3 +14,4 @@ try:
 except Exception as e:
     st.error(f"Ancora errore: {e}")
     st.info("Se vedi ancora 404, il link che hai incollato non è corretto o il file non è pubblicato.")
+
